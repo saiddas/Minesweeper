38747e5f3fef9322ba203deac72f4e70b43ed52e
Said Das <said.das@ozu.edu.tr>
2021-06-19 14:54:41 +0300

    Panels have been arranged roughly.

--- src/mineSweeperGame/MineSweeper.java
+++ src/mineSweeperGame/MineSweeper.java
@@ -1,6 +1,8 @@
 package mineSweeperGame;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class MineSweeper {
 	private static final int NUM_MINES = 100;
@@ -10,11 +12,20 @@ public class MineSweeper {
 		 JFrame frame = new JFrame("Mine Sweeper | # of mines: " + NUM_MINES);
 		 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		 
-		 MineSweeperGUI mainPanel =new MineSweeperGUI(SIZE, SIZE, NUM_MINES);
 		 MenuGUI menu = new MenuGUI();
+		 JPanel backgroundPanel = new JPanel();
+		 JPanel topPanel = new JPanel();
+		 MineSweeperGUI mainPanel =new MineSweeperGUI(SIZE, SIZE, NUM_MINES);
+		
+		 backgroundPanel.setLayout(null);
+		 backgroundPanel.add(topPanel);
+		 backgroundPanel.add(mainPanel);
+		 topPanel.setBounds(10, 10, 500, 30);
+		 mainPanel.setBounds(10, 50, 500, 500);
+		 topPanel.add(new JLabel("Label"));
 		 
 		 frame.setJMenuBar(menu);
-		 frame.add(mainPanel);
+		 frame.add(backgroundPanel);
 		 
 		 frame.setSize(1000, 600);
 		 frame.setVisible(true);
