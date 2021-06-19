5b705abf18a0805af9bd3d3534853fc785e35aba
Said Das <said.das@ozu.edu.tr>
2021-06-19 19:12:27 +0300

    TopPanel is added, now it can be specified.

---
+++ src/mineSweeperGame/TopPanel.java
@@ -0,0 +1,21 @@
+package mineSweeperGame;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.border.BevelBorder;
+
+public class TopPanel extends JPanel {
+	private Chronometer chronometer;
+	private MineCounter mineCounter;
+	private JButton smiley;
+	
+	public TopPanel() {
+		smiley = new JButton(new ImageIcon("src/iconSource/happy.jpg"));
+		smiley.setPressedIcon(new ImageIcon("src/iconSource/happyPressed.jpg"));
+		setLayout(null);
+		add(smiley);
+		smiley.setBounds(260, 10, 40, 40);
+		
+	}
+}
